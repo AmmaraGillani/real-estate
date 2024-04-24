ee02491750b0cf7f0b638ab57a64f64ca19f58b9
AmmaraGillani <ammara.gillani79@gmail.com>
2024-04-25 00:05:29 +0500

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,6 @@
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
+
 export const createListing = async(req, res, next) =>{
     try {
         const listing = await Listing.create(req.body);
@@ -11,12 +12,15 @@ export const createListing = async(req, res, next) =>{
 
 export const deleteListing = async(req, res, next) => {
     const listing = await Listing.findById(req.params.id);
+    
     if(!listing){
       return next(errorHandler(404, 'Listing not found!'));
     }
+
     if(req.user.id !== listing.userRef){
       return next(errorHandler(401,'You can only delete your own listings!'))
     }
+
     try {
       await Listing.findByIdAndDelete(req.params.id);
       res.status(200).json('listing has been deleted!');
@@ -26,22 +30,33 @@ export const deleteListing = async(req, res, next) => {
     };
 
     export const updateListing = async (req, res, next) => {
-      const listing = await Listing.findById(req.Params.id);
-      if(!listing){
-        return next(errorHandler(404, 'Listing not found'));
+      const listing = await Listing.findById(req.params.id);
+      if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
       }
-      
-      if(req.user.id !== listing.userRef) {
+      if (req.user.id !== listing.userRef) {
         return next(errorHandler(401, 'You can only update your own listings!'));
       }
+    
       try {
-        const updateListing = await Listing.findByIdAndUpdate(
+        const updatedListing = await Listing.findByIdAndUpdate(
           req.params.id,
           req.body,
-          { new:true}
+          { new: true }
         );
         res.status(200).json(updatedListing);
       } catch (error) {
         next(error);
       }
+    };
+    export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
     }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
