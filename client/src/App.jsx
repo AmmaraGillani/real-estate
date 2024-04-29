3b5e9e1dbc76995af354507b804a8296196c5b0f
AmmaraGillani <ammara.gillani79@gmail.com>
2024-04-29 14:27:57 +0500

    add image slider to listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import CreateListing from './pages/CreateListing';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
       <Route path = '/sign-up' element = {<SignUp />} />
       <Route path = '/sign-in' element = {<Signin />} />
       <Route path = '/about' element = {<About />} />
+      <Route path = '/listing/:listingId' element = {<Listing />} />
       <Route element = {<PrivateRoute />}>
         <Route path = '/profile' element = {<Profile />} />
         <Route path = '/create-listing' element = {<CreateListing />} />
